dbd5aa555746cf63d3a71efc8465d60836acfb54
Stephanie Otteson <vibrantimpact@myyahoo.com>
2026-07-12 12:24:43 -0600

    Initial Commit

---
+++ iOSApp4/iOSApp4App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp4App.swift
+//  iOSApp4
+//
+//  Created by stephanie otteson on 2026-07-12.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
